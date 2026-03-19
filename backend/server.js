0a82651d041b0e50629828ce7675b562210e4705
dafafadhilah <73998262+dafafadhilah@users.noreply.github.com>
2026-03-20 01:11:41 +0700

    all push

--- backend/server.js
+++ backend/server.js
@@ -469,8 +469,4 @@ app.put("/admin/orders/:id/status", async (req, res) => {
   res.json(data);
 });
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
